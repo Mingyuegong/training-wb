1545a91a6109c32a85de4527b0307469d101e152
Mingyuegong <125575211+Mingyuegong@users.noreply.github.com>
2023-05-28 20:55:37 +0800

    Update launch.py

--- launch.py
+++ launch.py
@@ -6,14 +6,19 @@ import importlib.util
 import shlex
 import platform
 import json
-
 from modules import cmd_args
 from modules.paths_internal import script_path, extensions_dir
+from pyngrok import ngrok
+public_url = ngrok.connect(7860).public_url
+print('Public URL:', public_url)
+
 
 commandline_args = os.environ.get('COMMANDLINE_ARGS', "")
 sys.argv += shlex.split(commandline_args)
 
 args, _ = cmd_args.parser.parse_known_args()
+
+
 
 python = sys.executable
 git = os.environ.get('GIT', "git")
